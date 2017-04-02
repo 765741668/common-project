35e8d0e73c47db9be729baab40af80667fac20b1
yangzhao <yangzhao_java@163.com>
2017-04-02 20:11:25 +0800

    StringUtil中添加生成随机数字+字母组合的字符串

--- src/main/java/com/app/utils/StringUtils.java
+++ src/main/java/com/app/utils/StringUtils.java
@@ -1,5 +1,6 @@
 package com.app.utils;
 
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -80,5 +81,31 @@ public class StringUtils extends org.springframework.util.StringUtils {
 			return true;
 		}
 		return false;
+	}
+
+	/**
+	 * 生成随机数字和字母
+	 * @param length
+	 * @return
+	 */
+	public String createStringRandom(int length) {
+
+		String val = "";
+		Random random = new Random();
+
+		//参数length，表示生成几位随机数
+		for(int i = 0; i < length; i++) {
+
+			String charOrNum = random.nextInt(2) % 2 == 0 ? "char" : "num";
+			//输出字母还是数字
+			if( "char".equalsIgnoreCase(charOrNum) ) {
+				//输出是大写字母还是小写字母
+				int temp = random.nextInt(2) % 2 == 0 ? 65 : 97;
+				val += (char)(random.nextInt(26) + temp);
+			} else if( "num".equalsIgnoreCase(charOrNum) ) {
+				val += String.valueOf(random.nextInt(10));
+			}
+		}
+		return val;
 	}
 }
